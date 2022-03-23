a61927d9a1fdd8693029142c43b197bd9ca4fae1
jcournut <jcournut@openstudio.fr>
2022-03-23 10:46:03 +0100

    fix multiple recaptcha invisible

--- Hook/HookManager.php
+++ Hook/HookManager.php
@@ -39,13 +39,44 @@ class HookManager extends BaseHook
 
         $captchaId= "recaptcha";
         $captchaCallback = "";
+        $type = "";
+
         if ($captchaStyle === 'invisible') {
             $captchaCallback = "data-callback='onCompleted'";
+            $type = "g-invisible";
             $captchaId .= '-invisible';
         }
 
-        $event->add("<div id='$captchaId' class='g-recaptcha' data-sitekey='$siteKey' $captchaCallback data-size='$captchaStyle'></div>");
+        if (null !== $event->getArgument('id')) {
+            $captchaId = $event->getArgument('id');
+        }
+
+        $event->add("<div id='$captchaId' class='g-recaptcha $type' data-sitekey='$siteKey' $captchaCallback data-size='$captchaStyle'></div>");
     }
 
+    public function loadRecaptcha(HookRenderEvent $event)
+    {
+        $siteKey = ReCaptcha::getConfigValue('site_key');
+        $captchaStyle = ReCaptcha::getConfigValue('captcha_style');
 
+        if ($captchaStyle !== 'invisible') {
+            $event->add($this->render(
+                'recaptcha-js.html',
+                [
+                    "siteKey" => $siteKey,
+                    "captchaStyle" => $captchaStyle,
+                ]
+            ));
+
+            return;
+        }
+
+        $event->add($this->render(
+            'recaptcha-js-invisible.html',
+            [
+                "siteKey" => $siteKey,
+                "captchaStyle" => $captchaStyle,
+            ]
+        ));
+    }
 }
