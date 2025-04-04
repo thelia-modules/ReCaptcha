476bae080fbbadb536c7e48a074bfa519bd0e164
Nicolas Barbey <nbarbey@openstudio.fr>
2025-04-04 17:04:19 +0200

    add listeners to contact, customer and newletter form

--- Action/ReCaptchaAction.php
+++ Action/ReCaptchaAction.php
@@ -5,16 +5,22 @@ namespace ReCaptcha\Action;
 use ReCaptcha\Event\ReCaptchaCheckEvent;
 use ReCaptcha\Event\ReCaptchaEvents;
 use ReCaptcha\ReCaptcha;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
+use Thelia\Core\Event\Contact\ContactEvent;
+use Thelia\Core\Event\Customer\CustomerCreateOrUpdateEvent;
+use Thelia\Core\Event\TheliaEvents;
+use Thelia\Form\Exception\FormValidationException;
+use Thelia\Model\Event\NewsletterEvent;
 
 class ReCaptchaAction implements EventSubscriberInterface
 {
     /** @var  Request */
     protected $request;
 
-    public function __construct(RequestStack $requestStack)
+    public function __construct(RequestStack $requestStack, private readonly EventDispatcherInterface $eventDispatcher)
     {
         $this->request = $requestStack->getCurrentRequest();
     }
@@ -47,10 +53,23 @@ class ReCaptchaAction implements EventSubscriberInterface
         }
     }
 
+    public function sendCaptchaEvent(): void
+    {
+        $checkCaptchaEvent = new ReCaptchaCheckEvent();
+        $this->eventDispatcher->dispatch($checkCaptchaEvent, ReCaptchaEvents::CHECK_CAPTCHA_EVENT);
+
+        if ($checkCaptchaEvent->isHuman() === false) {
+            throw new FormValidationException('Invalid captcha');
+        }
+    }
+
     public static function getSubscribedEvents()
     {
         return [
             ReCaptchaEvents::CHECK_CAPTCHA_EVENT => ['checkCaptcha', 128],
+            TheliaEvents::NEWSLETTER_SUBSCRIBE => ['sendCaptchaEvent', 256],
+            TheliaEvents::CUSTOMER_CREATEACCOUNT => ['sendCaptchaEvent', 256],
+            TheliaEvents::CONTACT_SUBMIT => ['sendCaptchaEvent', 256],
         ];
     }
 }
